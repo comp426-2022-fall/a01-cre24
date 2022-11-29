1a227154c23f8d77be82d3e9971a23323bfcb2c6
Ce'Niyah Ellison <69821183+cre24@users.noreply.github.com>
2022-11-29 16:40:02 -0500

    Made server.js file more organized

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 // Require http module
 const http = require('http');
+
 // Require fs module
 const fs = require("fs");
+
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist');
+
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2));
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || 3000;
+
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -18,6 +23,7 @@ fs.readFile('./public/index.html', 'utf-8', (err,data) => {
         console.error(err);
         return;
     }
+
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
@@ -35,7 +41,6 @@ fs.readFile('./public/index.html', 'utf-8', (err,data) => {
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
     server.listen(port, () => {
         console.log(`Server listening on port ${port}`)
     })
